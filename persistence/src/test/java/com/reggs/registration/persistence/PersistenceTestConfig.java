9ca79116c6a194c71e0a9ffb6db960b12c4ac572
Reagan Fwamba <youknowreaganfwambaa@gmail.com>
2026-07-06 13:09:03 +0300

    fix(persistence): configure Spring context and dependencies for repository tests

---
+++ persistence/src/test/java/com/reggs/registration/persistence/PersistenceTestConfig.java
@@ -0,0 +1,8 @@
+package com.reggs.registration.persistence;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenceTestConfig {
+    // This is just an anchor to tell Spring where the context starts
+}
